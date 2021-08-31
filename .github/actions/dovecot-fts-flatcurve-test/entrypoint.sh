5f8156a10c8ec160241d670b4c0c418f07be2e72
Michael M Slusarz <slusarz@noreply.users.github.com>
2021-08-31 10:28:02 -0600

    Add testing for prefix-only searching

--- .github/actions/dovecot-fts-flatcurve-test/entrypoint.sh
+++ .github/actions/dovecot-fts-flatcurve-test/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 ulimit -c unlimited
+
+echo "Testing RFC Compliant (substring) configuration"
 dovecot
-imaptest user=foo pass=pass test=/dovecot/fts-test
+if ! imaptest user=foo pass=pass test=/dovecot/imaptest/fts-test ; then
+	echo "ERROR: Failed test!"
+	exit 1
+fi
+
+echo
+echo "Testing prefix-only configuration"
+doveadm stop
+dovecot -c /dovecot/dovecot.conf.no_substring
+export IMAPTEST_NO_SUBSTRING=1
+if ! imaptest user=foo pass=pass test=/dovecot/imaptest/fts-test ; then
+	echo "ERROR: Failed test!"
+	exit 1
+fi
