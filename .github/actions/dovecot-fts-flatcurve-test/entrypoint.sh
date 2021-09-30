e76948340abb4a301beca40b5d610837b86e30e5
Michael M Slusarz <slusarz@users.noreply.github.com>
2021-09-30 11:31:36 -0600

    github actions: Test for MR #9

--- .github/actions/dovecot-fts-flatcurve-test/entrypoint.sh
+++ .github/actions/dovecot-fts-flatcurve-test/entrypoint.sh
@@ -1,21 +1,42 @@
 #!/usr/bin/env bash
+
+TESTUSER=user
+TESTPASS=pass
+DOVECOT_LOG=/var/log/dovecot.log
+
 ulimit -c unlimited
 
-echo "Testing RFC Compliant (substring) configuration"
-dovecot -c /dovecot/configs/dovecot.conf
-if ! imaptest user=foo pass=pass test=/dovecot/imaptest/fts-test ; then
-	echo "ERROR: Failed test!"
-	cat /var/log/dovecot.log
-	exit 1
-fi
+function restart_dovecot() {
+	doveadm stop &> /dev/null
+	rm -f $DOVECOT_LOG
+	dovecot -c $1
+}
 
-echo
-echo "Testing prefix-only configuration"
-doveadm stop
-dovecot -c /dovecot/configs/dovecot.conf.no_substring
+function run_imaptest() {
+	if ! imaptest user=$TESTUSER pass=$TESTPASS test=$1 ; then
+		echo "ERROR: Failed test!"
+		cat $DOVECOT_LOG
+		exit 1
+	fi
+}
+
+function run_test() {
+	echo
+	echo $1
+	restart_dovecot $2
+	run_imaptest $3
+}
+
+run_test "Testing RFC Compliant (substring) configuration" \
+	/dovecot/configs/dovecot.conf \
+	/dovecot/imaptest/fts-test
+
 export IMAPTEST_NO_SUBSTRING=1
-if ! imaptest user=foo pass=pass test=/dovecot/imaptest/fts-test ; then
-	echo "ERROR: Failed test!"
-	cat /var/log/dovecot.log
-	exit 1
-fi
+run_test "Testing prefix-only configuration" \
+	/dovecot/configs/dovecot.conf.no_substring \
+	/dovecot/imaptest/fts-test
+unset IMAPTEST_NO_SUBSTRING
+
+run_test "Testing GitHub Issue #9" \
+	/dovecot/configs/dovecot.conf.issue-9 \
+	/dovecot/imaptest/issue-9
