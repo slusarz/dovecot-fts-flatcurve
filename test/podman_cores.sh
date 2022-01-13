a36c7d9218d38bc8dc4e0c33fa13bc3943960382
Michael M Slusarz <slusarz@users.noreply.github.com>
2022-01-13 10:40:38 -0700

    Fix podman_cores.sh to actually work

--- test/podman_cores.sh
+++ test/podman_cores.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env
+#!/usr/bin/env /bin/sh
+# Run as root!
+# Usage: podman_cores.sh <podman_user> <podman_container>
 
-mkdir /tmp/cores
+mkdir -p /tmp/cores
 chmod 1777 /tmp/cores
 
 echo 2 >> /proc/sys/fs/suid_dumpable
 echo /tmp/cores/core.%u.%p >> /proc/sys/kernel/core_pattern
 
-podman run --rm -it --cap-add=ALL -v /cores:/cores --ulimit core=-1 --entrypoint /bin/bash $1
+runuser -u $1 -- podman run --rm -it --cap-add=ALL -v /tmp/cores:/cores --ulimit core=-1 --entrypoint /bin/bash $2
